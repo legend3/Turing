8216afea72cc9ab48e0482905e9a30e22bdf526e
legend3 <zhenguo50@126.com>
2020-04-26 04:07:43 +0800

    2020.04.26

--- 20-MySQL/Demo.py
+++ 20-MySQL/Demo.py
@@ -7,18 +7,16 @@ import pymysql
 '''
 @Author: LEGEND
 @since: 2020-04-25 13:07:55
-@lastTime: 2020-04-25 15:03:49
-@FilePath: \Turing\20-MySQL\Demo.py
+@lastTime: 2020-04-25 13:17:30
+@FilePath: \Turing\18-python+shell\20-MySQL\Demo.py
 @Description: 
 @version: 
 '''
 
 
 def test_mysql(connection):
-    assert isinstance(connection, pymysql)
     try:
-        cur = pymysql.connect().cursor()
-        pymysql.
+        cur = con.
     except Exception as e:
         print("操作数据库异常：", e)
     else:
@@ -28,9 +26,9 @@ def test_mysql(connection):
 @pytest.fixture()
 def connection():
     try:
-        con = pymysq.connect(host='localhost', user='root', password='root', port=3306, db='mydata', charset='utf8mb4')
+        con = pymysql.connect(host='localhost', user='root', password='root', port=3306, db='mydata', charset='utf8mb4')
         print("连接数据路成功！")
-        return con
+        return con;
     except Exception as e:
         print("数据库连接异常", e)
     finally:

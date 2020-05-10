c0a4067715b4737f78034f8efaf2b86c4daf0b9f
legend3 <zhenguo50@126.com>
2020-05-11 05:26:46 +0800

    2020.05.11

--- 2-高级-包管理/rootTest.py
+++ 2-高级-包管理/rootTest.py
@@ -8,43 +8,46 @@ import sys
 '''
 @Author: LEGEND
 @since: 2020-05-11 01:54:03
-@lastTime: 2020-05-11 04:21:28
+@lastTime: 2020-05-11 05:26:21
 @FilePath: \Turing\2-高级-包管理\rootTest.py
-@Description: 
+@Description: (不)同级包之间的导包
 @version: 
 '''
 
 
+# params = importlib.import_module('b.c.c') #绝对导入
+# params_ = importlib.import_module('.c.c',package='b') #相对导入
 
-# curPath = os.path.abspath(__file__)
-# rootPath = os.path.split(curPath)[0] + '/../'
-# print(os.path.abspath(rootPath))
+# # 对象中取出需要的对象
+# params.args #取出变量
+# params.C  #取出class C
+# params.C.c  #取出class C 中的c 方法
 
-# print(os.path.dirname(curPath))  #
-# sys.path.append(os.path.dirname(curPath))
+"""
+说明：(pydev在运行时会把当前工程的所有文件夹路径都作为包的搜索路径)，而命令行默认只是搜索当前路径(和系统包，没有项目工程目录路径！)。
+"""
+# importlib.reload(sys)  # (永久)用于重新载入之前载入的模块。(载入过在sys.path中加入过'python基础',sys.path中就一直存在'python基础'包模块)
+# sys.getdefaultencoding()  # 获悉设置编码
+print(sys.path)
 
-# reload(sys)  # 用于重新载入之前载入的模块。(载入过在sys.path中加入过'python基础',sys.path中就一直存在'python基础'包模块)
-# sys.setdefaultencoding('utf-8')  # # 设置编码
-# sys.path.append(sys.path[0] + "/../")
-sys.path.append(os.path.abspath(sys.path[0] + "/../"))
-# print("这里", os.path.abspath(sys.path[0] + "/../"))
+"""第一种方式"""
+curPath = os.path.abspath(__file__)
+rootPath = os.path.split(curPath)[0] + '/../'
+sys.path.append(rootPath)  # 
+# sys.path.append(os.path.abspath(sys.path[0] + "/../"))
+# print(os.path.dirname(curPath))
+print(sys.path)
 
-# sys.path.append(os.getcwd())  # 获取的是工作空间的路径
-
-# s = importlib.import_module('.01','2-高级-包管理')
-s = importlib.import_module('2-高级-包管理.01')
-                                                # params = importlib.import_module('b.c.c') #绝对导入
-                                                # params_ = importlib.import_module('.c.c',package='b') #相对导入
-
-                                                # # 对象中取出需要的对象
-                                                # params.args #取出变量
-                                                # params.C  #取出class C
-                                                # params.C.c  #取出class C 中的c 方法
+"""第二种方式"""
+# sys.path.append(os.getcwd())  # 获取工作空间的路径
 
 
-s.Student()
+# from pythonTest import test
+# test.Student()
 
-# from i import p01
-# tuling.Student()
 
+# s = importlib.import_module('.01','1-python基础.2-OOP')
+# s = importlib.import_module('1-python基础.2-OOP.01')
+
+# s.Student()
 
---
+++ pythonTest/OOP/01.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+@Author: LEGEND
+@since: 2019-12-22 11:35:21
+@lastTime: 2020-04-25 13:06:45
+@FilePath: \Turing\1-python基础\2-OOP\01.py
+@Description: 
+@version: 
+'''
+
+
+'''
+    定以一个学生类，用来形容学生
+'''
+
+
+# 定义一个空的类
+class Student():
+    # 一个空类，pass代表直接跳过
+    # 此处pass必须有
+    pass
+
+# 定义一个对象
+mingyue = Student()
+
+# 再定义一个类，用来描述听Python的学生
+class PythonStudent():
+    # 用None给不确定的值赋值
+    name = None  # 补充：静态变量：1.用通过类名或对象实例访问静态变量都是合法的 2.必须通过类型访问才能修改静态变量的值（可以用id()查看静态变量id验证！）
+    age = 18
+    course = "Python"
+
+    # 需要注意
+    # 1. def doHomework的缩进层级
+    # 2. 系统默认由一个self参数
+    def doHomework(self):
+        print("I 在做作业")
+
+        # 推荐在函数末尾使用return语句
+        return None
+
+# 实例化一个叫yueyue的学生，是一个具体的人
+yueyue = PythonStudent()
+print(yueyue.name)  # 补充：用类实例访问静态变量
+print(yueyue.age)
+# 注意成员函数的调用没有传递进入参数
+yueyue.doHomework()
+PythonStudent.name  # 补充：通过类名访问静态变量

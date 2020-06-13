ba9ef651f8af1bf5630f54ab439a94643ec20fa2
legend3 <zhenguo50@126.com>
2020-06-14 01:12:04 +0800

    2020.06.13

--- Pytest_learning/Python Testing with pytest/pytest_parallel/tests/testdemo.py
+++ Pytest_learning/Python Testing with pytest/pytest_parallel/tests/testdemo.py
@@ -4,9 +4,9 @@
 '''
 @Author: LEGEND
 @since: 2020-06-09 23:53:36
-@lastTime: 2020-06-10 00:08:19
+@lastTime: 2020-06-11 21:09:03
 @LastAuthor: Do not edit
-@FilePath: \Turing\Pytest_learning\Python Testing with pytest\pytest-parallel\tests\testdemo.py
+@FilePath: \Turing\Pytest_learning\Python Testing with pytest\pytest_parallel\tests\testdemo.py
 @Description: pytest-parallel支持python3.6及以上版本，如果是想做多进程并发的需要在linux平台或mac上做，
                 windows上不起作用即(workers永远=1)，
                     如果是做多线程的Linux/Mac/Windows平台都支持，进程数为workers设置的值。
@@ -21,4 +21,4 @@ import pytest
 
 @pytest.mark.parametrize('items', [1,2,3])
 def test_parallel(items):
-    print(items)    
+    print(items)

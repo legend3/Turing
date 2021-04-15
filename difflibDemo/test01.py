cf897ea489ad30a4f462e03406c474c6ff590d4d
legend3 <zhenguo50@126.com>
2021-04-16 03:13:44 +0800

    2021.04.16 difflib模块

--- difflibDemo/test01.py
+++ difflibDemo/test01.py
@@ -4,12 +4,14 @@
 '''
 Author: LEGEND
 since: 2021-04-13 23:21:28
-lastTime: 2021-04-13 23:47:51
+lastTime: 2021-04-16 03:13:20
 LastAuthor: Do not edit
 FilePath: /Turing/difflibDemo/test01.py
-Description: https://docs.python.org/zh-cn/3/library/difflib.html
-https://cloud.tencent.com/developer/article/1568500
-https://blog.csdn.net/IMW_MG/article/details/78149574
+Description: 
+    https://docs.python.org/zh-cn/3/library/difflib.html
+    https://cloud.tencent.com/developer/article/1568500
+    https://blog.csdn.net/IMW_MG/article/details/78149574
+    https://www.programcreek.com/python/example/1084/difflib.Differ
 version: 
 '''
 
@@ -29,6 +31,6 @@ text2_line=text2.splitlines()
 
 d = difflib.Differ() #创建Differ()对象
 
-diff = d .compare(text1_line,text2_line)
+diff = d.compare(text1_line,text2_line)
 
 print('\n'.join(list(diff)))

f4c5b1ca55569ef42594ce11d631a938bf20a029
legend3 <zhenguo50@126.com>
2021-12-18 17:23:41 +0800

    Jobs更新

--- APSchedulerDemo/Jobs.py
+++ APSchedulerDemo/Jobs.py
@@ -4,9 +4,9 @@
 '''
 Author: LEGEND
 since: 2021-12-18 02:15:01
-lastTime: 2021-12-18 16:53:53
+lastTime: 2021-12-18 17:10:10
 LastAuthor: Do not edit
-FilePath: /Turing/APSchedulerDemo/demo.py
+FilePath: /Turing/APSchedulerDemo/Jobs.py
 Description: 区分scheduler对象的占有线程, thread APScheduler or thread main?
 version: 
 '''
@@ -20,53 +20,56 @@ import threading
 #     return True
 
 scheduler = None
-class S():
+class AddJobs():
     def __init__(self):
         global scheduler
-        self.res = None
+        self.response = None
         scheduler = BackgroundScheduler()  # 调度器
         # self.job = scheduler.add_job(fun,trigger='interval',seconds=2,id='myJobId') # 添加job
         # scheduler.start()  # 初始化，启动调度器
     
-    def getResult(self):
-        return self.res
+    def getResponse(self):
+        return self.response
     
     def listener(self, event):
-        self.res = event.retval
+        self.response = event.retval
         if event.exception:
-            print('The job crashed :(')
+            print(event.exception)
         else:
-            if self.res > 2:
+            if self.response > 2:
                 # print("定时任务线程--", threading.currentThread().getName())  # 当前线程-- APScheduler
+                # 运行中删除job
                 # self.scheduler.remove_job('myJobId')  # thread APScheduler的scheduler删除job
                 scheduler.remove_job('myJobId')  # thread main的scheduler删除job    (全局变量由main线程操作)
                 print("my job is remove: ", scheduler.get_job('myJobId'))
 
-    def setJob(self, fun):
+    def setJobs(self, fun):
+        # 添加jobs
         scheduler.add_job(
             fun,
             trigger='interval',
 	        seconds=2,
             id='myJobId'
         )
+        # 添加监听器
         scheduler.add_listener(self.listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)  # 监听器
 
     def start(self):
         scheduler.start()
 
-    # def shut(self):
-    #     scheduler.remove_all_jobs()
+    def shut(self):
+        scheduler.remove_all_jobs()
 
-    def remove(self):
-        scheduler.remove_job('myJobId')
+    def remove(self, jobId):
+        scheduler.remove_job(jobId)
 
 
 # if __name__ == "__main__":
 #     s = S()
-#     s.setJob(fun)
+#     s.setJobs(fun)
 #     s.start()
-#     print(s.getResult())
-#     time.sleep(3)
+#     print(s.getResponse())
+#     time.sleep(3)  # 等待接口响应
 #     print("当前线程--", threading.currentThread().getName())  # 当前线程-- MainThread
-#     s.remove()  # (要等到BackgroundScheduler线程执行完job)要用主线程删除
-#     time.sleep(60)
+#     s.remove('myJobId')  # (要等到BackgroundScheduler线程执行完job)要用主线程删除
+#     time.sleep(60)  # 保持main线程运行状态
--- APSchedulerDemo/test_apscheduler.py
+++ APSchedulerDemo/test_apscheduler.py
@@ -6,7 +6,7 @@ import pytest
 '''
 Author: LEGEND
 since: 2021-05-18 01:10:13
-lastTime: 2021-12-18 16:53:27
+lastTime: 2021-12-18 17:19:19
 LastAuthor: Do not edit
 FilePath: /Turing/APSchedulerDemo/test_apscheduler.py
 Description: 
@@ -15,8 +15,8 @@ version:
 import sys, os
 import time
 import threading
-# sys.path.append(os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__)[0])), './'))
-from APSchedulerDemo.demo import S
+# sys.path.append(os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__)[0])), '../'))
+from APSchedulerDemo.Jobs import AddJobs
 
 a = 0
 def test():
@@ -26,10 +26,10 @@ def test():
 
 @pytest.mark.parametrize('case', [1,2])
 def test01(case):
-    s = S()
-    s.setJob(test)
-    s.start()
+    addJobs = AddJobs()
+    addJobs.setJobs(test)
+    addJobs.start()
     time.sleep(5)  # 接口响应时间
-    print(s.getResult())
+    print(addJobs.getResponse())
     # print("当前线程--", threading.currentThread().getName())  # 当前线程-- MainThread
     # s.remove()  # (要等到BackgroundScheduler线程执行完job)要用主线程删除
